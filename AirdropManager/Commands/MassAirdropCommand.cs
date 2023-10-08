f1367903f77e88c515bcb618880670dd8ab40645
liebesleid-eth <liebesleid99@gmail.com>
2023-10-08 16:41:24 -0700

    split massAirdrop into it's own command

--- AirdropManager/Commands/MassAirdropCommand.cs
+++ AirdropManager/Commands/MassAirdropCommand.cs
@@ -5,23 +5,23 @@ using System.Collections.Generic;
 
 namespace RestoreMonarchy.AirdropManager.Commands
 {
-    public class AirdropCommand : IRocketCommand
+    public class MassAirdropCommand : IRocketCommand
     {
         public void Execute(IRocketPlayer caller, params string[] command)
         {
-            AirdropManagerPlugin.Instance.CallAirdrop(false);
-            Logger.Log(AirdropManagerPlugin.Instance.Translate("SuccessAirdrop"), ConsoleColor.Yellow);
+            AirdropManagerPlugin.Instance.CallAirdrop(true);
+            Logger.Log(AirdropManagerPlugin.Instance.Translate("SuccessMassAirdrop"), ConsoleColor.Yellow);
         }
 
-        public string Help => "Calls in airdrop";
+        public string Help => "Calls in mass airdrop";
 
-        public string Name => "airdrop";
+        public string Name => "massairdrop";
 
         public string Syntax => string.Empty;
 
         public List<string> Aliases => new List<string>();
 
-        public List<string> Permissions => new List<string>() { "airdrop" };
+        public List<string> Permissions => new List<string>() { "massairdrop" };
 
         public AllowedCaller AllowedCaller => AllowedCaller.Both;
     }
